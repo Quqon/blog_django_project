1c76c5a66eade924a02372891808617d6a41fc7d
Quqon <wjddlsgh0203@gmail.com>
2023-01-12 21:26:50 +0900

    Post 모델의 필드값 보여주기

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+
 
 def index(request):
+    posts = Post.objects.all()
+
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
     )
