5844ec6c8a59daf390db564eb1bfdddd06e4f088
Quqon <wjddlsgh0203@gmail.com>
2023-01-17 23:02:23 +0900

    CBV로 블로그 포스트 목 록페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
-
-def index(request):
-    posts = Post.objects.all()
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    template_name = 'blog/index.html'
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
